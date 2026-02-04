0717cc3b1c6c525bac44d87e9ef05b3c45390855
ruan-lopes16 <sruan507@gmail.com>
2026-02-03 22:17:04 -0300

    numbers

---
+++ 2-operators/js/scripts.js
@@ -0,0 +1,17 @@
+// 1 - Number
+console.log(typeof 2)
+console.log(typeof -2)
+console.log(typeof 2.5)
+
+// 2 - arithmetic operations
+console.log(2 + 4)
+console.log(8 - 7)
+console.log(9 * 3)
+console.log(6 / 2)
+console.log(5 + 4 * 2)
+
+// 3 - special number
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(12 * "wasd")
+console.log(typeof NaN)
