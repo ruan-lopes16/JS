2568ecd21227d2fb482be94f0acc51c67baae5bb
ruan-lopes16 <sruan507@gmail.com>
2026-02-03 22:33:49 -0300

    Strings

--- 2-operators/js/scripts.js
+++ 2-operators/js/scripts.js
@@ -15,3 +15,19 @@ console.log(typeof Infinity)
 console.log(typeof -Infinity)
 console.log(12 * "wasd")
 console.log(typeof NaN)
+
+// 4 - string
+console.log("A text")
+console.log(`More one`)
+console.log('Another text')
+
+// 5 - special character
+console.log("Testing the \n line break")
+console.log("Tab \t spacing")
+
+// 6 - concatenation
+console.log("Hi," + " how are" + " you?")
+
+// 7 - text interpolation - template String
+console.log(`The sum of 2 + 2 is: ${2 + 2}`)
+console.log(`We can run anything here: ${console.log("Test")}`)
