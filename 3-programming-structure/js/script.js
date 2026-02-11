14d45d63020241307f74491ee776dda5c7fe3067
ruan-lopes16 <sruan507@gmail.com>
2026-02-10 21:33:22 -0300

    more about variables

--- 3-programming-structure/js/script.js
+++ 3-programming-structure/js/script.js
@@ -13,3 +13,25 @@ console.log(age)
 ////////////
 
 console.log(typeof name, typeof age)
+
+//////////////
+
+// 2 - more about variable
+// let 2test = "invalid"
+// let @test = "invalid"
+
+let a = 10, 
+    b = 20,
+    c = 30
+
+console.log(a, b, c)
+
+// caseSensitive
+const fullName = "Ruan Lopes"
+const FullName = "Ana Clara Silva"
+console.log(fullName) 
+console.log(FullName)
+
+let _test = "ok"
+let $test = "ok, ok"
+console.log(_test, $test)
