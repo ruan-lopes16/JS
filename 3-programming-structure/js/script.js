68b8e69fd26a030ba17107202631a34662b43ae3
ruan-lopes16 <sruan507@gmail.com>
2026-02-10 21:19:34 -0300

    variables

---
+++ 3-programming-structure/js/script.js
@@ -0,0 +1,15 @@
+// 1 - variables
+let name = "Ruan"
+console.log(name)
+
+name = "Ana Clara"
+console.log(name)
+
+//////////////
+
+const age = 21
+console.log(age)
+
+////////////
+
+console.log(typeof name, typeof age)
