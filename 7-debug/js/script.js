0363b3ecd59e2b30bebb52e0ec29b53ff0d5f553
ruan-lopes16 <sruan507@gmail.com>
2026-04-06 21:39:07 -0300

    console.log

--- 7-debug/js/script.js
+++ 7-debug/js/script.js
@@ -2,11 +2,31 @@
 "use strict"
 
 // não recomendado declarar variaveis sem 'var', 'let' ou 'const'
-const opa = "test"
+// const opa = "test"
 
 // não possi usar undefined para definir variaveis
-const undefined = 10
+// const undefined = 10
 
 // não posso deletar prop de um array
-delete [].length 
+//delete [].length 
+
+// 2 - console.log
+let a = 1
+let b = 2
+
+if (a == 1) {
+    a = b + 2
+}
+console.log(a)
+
+for(let i = 0; i < b; i++){
+    a = a + 2
+    console.log(a)
+}
+
+if (a > 5) {
+    a = 25
+}
+
+console.log(a)
 
