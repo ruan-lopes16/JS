72c01edf166b1a878b5d098c62528d4559a08877
ruan-lopes16 <sruan507@gmail.com>
2026-04-06 21:38:51 -0300

    strict

---
+++ 7-debug/js/script.js
@@ -0,0 +1,12 @@
+// 1 - strict
+"use strict"
+
+// não recomendado declarar variaveis sem 'var', 'let' ou 'const'
+const opa = "test"
+
+// não possi usar undefined para definir variaveis
+const undefined = 10
+
+// não posso deletar prop de um array
+delete [].length 
+
