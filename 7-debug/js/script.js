da4f19973bbfdaa472163e96959d8ca5d56c6c28
ruan-lopes16 <sruan507@gmail.com>
2026-04-07 21:21:47 -0300

    debugger

--- 7-debug/js/script.js
+++ 7-debug/js/script.js
@@ -30,3 +30,23 @@ if (a > 5) {
 
 console.log(a)
 
+// 3 - debugger
+let c = 1;
+let d = 2;
+
+if(c == 1){
+    c = d + 2
+}
+
+// ativando o debugger no navegador
+debugger;
+
+for(let i = 0; i < d; i++){
+    c = c + 2
+}
+
+console.log("executou o loop");
+
+if (c > 5){
+    c = 25
+}
