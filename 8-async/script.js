fe6ca4bd6f7b4281fcc41166082c6e50540a1ca6
ruan-lopes16 <sruan507@gmail.com>
2026-05-05 21:29:52 -0300

    interval

--- 8-async/script.js
+++ 8-async/script.js
@@ -3,6 +3,18 @@ console.log("has not yet executed")
 
 setTimeout(function() {
     console.log("asynchronous request")
+    console.log("--------------------------------------")
 }, 2000)
 
 console.log("has not yet executed 2 ")
+
+// 2 - setInterval
+console.log("has not yet executed")
+
+/*setInterval(function () {
+    console.log("asynchronous interval")
+    console.log("--------------------------------------")
+}, 3000)*/
+
+console.log("has not yet executed 2 ")
+console.log("--------------------------------------")
